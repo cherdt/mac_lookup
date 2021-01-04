98e1e1728fed71ed9521aae4370851d02b2a4dd5
Chris Herdt <cherdt@gmail.com>
2021-01-04 17:04:32 -0600

    Add mac_lookup source

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mac_lookup
 Lookup the organization (OUI) associated with a MAC address
+
+This is based on Laura F's Python mac_vendor tool. I wanted to create the same using Go as an exercise/practice using Go.
---
+++ src/mac_lookup.go
@@ -0,0 +1,41 @@
+package main
+import ( 
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"encoding/json"
+	"os"
+)
+
+func main() {
+
+	type MacAddress struct {
+                Mac string "mac"
+                Company string "company"
+		Address string "address"
+		MacStart string "mac_start"
+		MacEnd string "mac_end"
+		MacType string "type"
+		Country string "country"
+        }
+	var m MacAddress
+
+	resp, err := http.Get("https://api.maclookup.app/v1/macs/" + os.Args[1])
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+	    fmt.Println(string(body))
+	}
+
+	if json.Valid(body) != true {
+		fmt.Println("Response was not valid JSON")
+	} else {
+		// Unmarshal (deserialize) the JSON response
+		jsonerr := json.Unmarshal(body, &m)
+		if jsonerr != nil {
+			fmt.Println(jsonerr)
+		} else {
+			fmt.Println(m.Company)
+		}
+	}
+}
